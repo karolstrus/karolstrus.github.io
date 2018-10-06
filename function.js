08a3d5a4ccc6356993494ac9108482ec0e52a73c
Karol Strus <karol.strus99@gmail.com>
2018-10-06 21:13:05 +1000

    New website

--- function.js
+++
@@ -1,8 +0,0 @@
-function canvasLine() {
-    let c = document.getElementById("myCanvas");
-    let ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.moveTo(35, 65);
-    ctx.lineTo(265, 65);
-    ctx.stroke();
-}
